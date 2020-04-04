c8c6e6588b8c3c20b1817c580b1576ab2000ffc9
Oscar <olongino@grupoasesores.com.mx>
2020-04-04 10:52:02 -0600

    1.- Se agrega el componente Logger.js

--- commons/Logger.js
+++ commons/Logger.js
@@ -5,13 +5,64 @@
 
 /**
  * @author: Leonardo Ivan Torres Ochoa [30/03/2020]
- * @updated: ---
+ * @updated: Oscar Longino [31/03/2020]
  * @description: library for Liverpool Analytics Tagging
  * @since-version: 1.0
  */
+
 module.exports = class Logger {
+
   // eslint-disable-next-line class-methods-use-this
-  log(message) {
-    console.log('log::{}', message);
+  constructor(debugMode, fileName){
+    this.debugMode = debugMode;
+    this.fileName = fileName
+    console.log('constructor Logger: '+this.debugMode, " "+ this.fileName);
+   
   }
+
+   /** Set de trace for operation */
+   info(message, obj) {
+    if (this.debugMode) {
+        if(obj != undefined){
+            console.log("[ " + new Date() + " ] - " + "[ " + this.fileName + " ] - " +  "[ INFO ] - " + message, obj);
+        }else{
+            console.log("[ " + new Date() + " ] - " + "[ " + this.fileName + " ] - " +  "[ INFO ] - ", message);
+        }
+        
+      }
+  }
+
+  /** Set de trace for operation */
+  error(message, obj) {
+      if (this.debugMode) {
+        if(obj != undefined){
+            console.log("[ " + new Date() + " ] - " + "[ " + this.fileName + " ] - "  +  "[ ERROR ] - " + message, obj);
+        }else{
+            console.log("[ " + new Date() + " ] - " + "[ " + this.fileName + " ] - "  +  "[ ERROR ] - ", message);
+        }
+      }
+  }
+
+  /** Set de trace for operation */
+  warn(message, obj) {
+      if (this.debugMode) {
+        if(obj != undefined){
+            console.log("[ " + new Date() + " ] - " + "[ " + this.fileName + " ] - "  +  "[ WARNING ] - " + message, obj);
+        }else{
+            console.log("[ " + new Date() + " ] - " + "[ " + this.fileName + " ] - "  +  "[ WARNING ] - ", message);
+        }
+      }
+  }
+
+  /** Set de trace for operation */
+  fatal(message, obj) {
+      if (this.debugMode) {
+        if(obj != undefined){
+            console.log("[ " + new Date() + " ] - " + "[ " + this.fileName + " ] - "  +  "[ FATAL ] - " + message, obj);
+        }else{
+            console.log("[ " + new Date() + " ] - " + "[ " + this.fileName + " ] - "  +  "[ FATAL ] - ", message);
+        }
+      }
+  }
+
 };
--- test.js
+++ test.js
@@ -1,6 +1,53 @@
-const assert = require('assert');
+/**
+ * Copyright (c) 2020 - Liverpool. All rights reserved
+ * Grupo de Asesores Profesionales en Servicios de Integraci�n {GAPSI} - CDMX - 2020
+ */
 
-let taggeo = require('./index');
+/**
+ * @author: Leonardo Ivan Torres Ochoa [30/03/2020]
+ * @updated: Oscar Longino [31/03/2020]
+ * @description: test file of lib
+ * @since-version: 1.0
+ */
 
-taggeo = new taggeo.Analytics();
-assert.ok(typeof taggeo.healthCheck === 'function');
+console.log("========================================================================================");
+console.log(" ");
+
+const AnalyticsTagging = require('./index.js');
+const API_KEY = 'bf6c0c4a-7693-4b1b-9f3f-c7d7cc3b38d2';
+const analyticsTagging = new AnalyticsTagging.Analytics(API_KEY);
+
+    let listProducts = [];
+    let product = {
+      name: 'Chamarra Puma roja',
+      id: '1084082111',
+      category: 'El',
+      variant: 'N/A',
+      price: '2379.15',
+      brand: 'PUMA',
+      quantity: 1,
+      size: 'CH',
+      color: 'ROJO',
+      vendor: 'liverpool',
+      skuSpecific: '1084082127',
+      material: 'Poliester',
+      texture: '',
+      packBelongs: 'N',
+      discount: '2799.0',
+      originalPrice: '2379.15'
+    };
+    listProducts.push(product);
+    let liverpoolLayer = {
+      event: 'addToCart',
+      products: listProducts
+    };
+setTimeout(
+    function (){
+        analyticsTagging.publish(liverpoolLayer);
+    }, 2000
+)
+
+
+console.log(" ");
+console.log(" ");
+console.log("========================================================================================");
